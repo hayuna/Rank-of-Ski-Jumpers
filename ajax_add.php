9138c74d8811e4df655146c345ecae291530dccb
mras2303 <marek.kowalonek@gmail.com>
2017-03-26 00:47:18 +0100

    Fix adding records

--- ajax_add.php
+++ ajax_add.php
@@ -4,6 +4,6 @@
 	$nationality = $_POST['nationality'];
 	$age = $_POST['age'];
 	$points = $_POST['points'];
-	$query = mysql_query("INSERT INTO jumpers VALUES (NULL, '".$name."', '".$nationality."', ".$age.", ".$points.")");
+	$query = mysql_query("INSERT INTO jumpers (id, name, nationality, age, points) VALUES (NULL, '".$name."', '".$nationality."', ".$age.", ".$points.")");
 
 ?>
