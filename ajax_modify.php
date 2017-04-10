1ba5fde00db9abe5749ff7eed63ef869046206f4
Unknown <marek.kowalonek@gmail.com>
2017-04-10 23:14:29 +0200

    Add $image to modify in query

--- ajax_modify.php
+++ ajax_modify.php
@@ -5,6 +5,7 @@
 	$nationality = $_POST['nationality'];
 	$age = $_POST['age'];
 	$points = $_POST['points'];
-	$query = mysql_query("UPDATE jumpers SET name = '".$name."', nationality = '".$nationality."', age = ".$age.", points = ".$points." WHERE id = ".$id);
+	$image = $_POST['image'];
+	$query = mysql_query("UPDATE jumpers SET name = '".$name."', nationality = '".$nationality."', age = ".$age.", points = ".$points.", img = '".$image."' WHERE id = ".$id);
 
 ?>
