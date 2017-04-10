2dfd8cc0a627716467aebcf794e27a21dbc14d78
Unknown <marek.kowalonek@gmail.com>
2017-04-10 23:13:57 +0200

    Add image field to query

--- ajax_add.php
+++ ajax_add.php
@@ -4,6 +4,7 @@
 	$nationality = $_POST['nationality'];
 	$age = $_POST['age'];
 	$points = $_POST['points'];
-	$query = mysql_query("INSERT INTO jumpers (id, name, nationality, age, points) VALUES (NULL, '".$name."', '".$nationality."', ".$age.", ".$points.")");
-
-?>
+	$image = $_POST['image'];
+	if(strlen($image)<1){$image = "img/default.png";}
+	$query = mysql_query("INSERT INTO jumpers (id, name, nationality, age, points, img) VALUES (NULL, '".$name."', '".$nationality."', ".$age.", ".$points.", '".$image."')");
+	?>
